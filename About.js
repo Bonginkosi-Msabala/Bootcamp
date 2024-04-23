7ba3c8a1e7b790efad614ccb154d36495ada6ec2
Bonginkosi-Msabala <166005526+Bonginkosi-Msabala@users.noreply.github.com>
2024-04-23 18:55:28 +0200

    Javascript version  2

---
+++ About.js
@@ -0,0 +1,8 @@
+const page =document.getElementById('body')
+
+const divContainer = document.createElement("div")
+divContainer.classList.add("about")
+divContainer.innerHTML = ` 
+`
+
+divContainer.appendChild(divContainer)
